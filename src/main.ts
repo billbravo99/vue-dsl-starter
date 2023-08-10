8134a36fa32a2413c2e87042c9a8fd901a9178cf
billbravo99 <billy.maulana@kpvendor.id>
2023-08-10 15:58:53 +0700

    chore: pointing dsl style

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { setupThemesAndComponents } from 'kbfmf-libraries-beta'
-import '../node_modules/kbfmf-libraries-beta/dist/assets/style.css'
+import 'kbfmf-libraries-beta/dist/style.css'
 import App from './App.vue'
 import router from './router'
 
